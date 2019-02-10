d0e4a4a8f273bc2dcea3b558539c7f271e3294e7
Vadym Sereda <flash.brovary@gmail.com>
2019-02-11 01:30:20 +0200

    genesis test task - first commit

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,11 @@
+
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', {
+    type: 'carousel',
+    perView: 1
+}).mount();
+new Glide('.glide2', {
+    type: 'carousel',
+    perView: 1
+}).mount();
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/assets/pug/index.pug"
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: 'pug-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 3000,
+                    name: 'assets/img/[name].[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 5000,
+                    name: 'assets/fonts/[name].[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    }
+};
